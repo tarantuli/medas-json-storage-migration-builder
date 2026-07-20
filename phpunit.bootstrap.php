4c9c19d50efe6fb85ea1e06c38e1b8826bf46dd1
Patrick Morpey <patrick@morphp.nl>
2026-07-20 10:33:12 +0200

    Updated dependencies to latest versions and adjusted code for `medas-core` v4 compatibility.

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-use Medas\ObjectInstantiator\ObjectInstantiator;
 use Medas\JsonStorageMigrationBuilder\JsonStorageMigrationBuilderPackage;
-use Medas\ServiceManager\{ServiceConfig, ServiceManager};
+use Medas\ObjectInstantiator\ObjectInstantiator;
+use Medas\ServiceManager\{ServiceConfigBuilder, ServiceManager};
 
 chdir(__DIR__);
 
-new ServiceManager(function (): ServiceConfig {
-    $config = new ServiceConfig(ObjectInstantiator::class);
+new ServiceManager(function (): ServiceConfigBuilder {
+    $config = new ServiceConfigBuilder(ObjectInstantiator::class);
 
     $config->addPackages([
         JsonStorageMigrationBuilderPackage::instance(),
--- src/MigrationBuilder.php
+++ src/MigrationBuilder.php
@@ -74,4 +74,3 @@ PHP;
         return $storage instanceof StorageDirectory;
     }
 }
-
